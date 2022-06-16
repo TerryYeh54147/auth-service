f58a3f5c9bae3ab9dac892d9a4cb7ca013b67487
terry54147 <0930164@gmail.com>
2022-06-16 20:13:19 +0800

    define the fieldsets and filter list of the account, change the AccountAdmin inherit class

--- auth/account/admin.py
+++ auth/account/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 from .models import Account
 
-class AccountAdmin(admin.ModelAdmin):
-    list_display=('username', 'email', 'phone', 'created_at', 'last_edit_at')
-    search_fields=('username', 'email', 'phone')
+
+class AccountAdmin(UserAdmin):
+    list_display = ('username', 'role', 'email', 'phone',
+                    'created_at', 'last_edit_at')
+    search_fields = ('uid', 'username', 'email', 'phone')
+    list_filter = ['role', 'is_staff', 'is_superuser']
+    fieldsets = UserAdmin.fieldsets
+
 
 admin.site.register(Account, AccountAdmin)
