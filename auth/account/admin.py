00f8b78f0b4ee59eb57b520fd0a7b63afec1d381
terry54147 <0930164@gmail.com>
2022-06-16 18:51:35 +0800

    define the list_display and search_fields of the Account in the admin

--- auth/account/admin.py
+++ auth/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Account
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display=('username', 'email', 'phone', 'created_at', 'last_edit_at')
+    search_fields=('username', 'email', 'phone')
+
+admin.site.register(Account, AccountAdmin)
