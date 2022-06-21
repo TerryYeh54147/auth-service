29383c8ba834292e822508f473a2726eb8d5848b
terry54147 <0930164@gmail.com>
2022-06-21 19:54:54 +0800

    init account model again and reset migration file

--- auth/account/models.py
+++ auth/account/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 class Account(AbstractUser):
     ROLES=(('ower', '擁有者'), ('maintainer', '維護者'), ('guest', '訪客'))
 
-    uid = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     role=models.CharField(choices=ROLES, max_length=20, default='guest')
     phone = models.CharField(max_length=10, blank=True)
     last_edit_at=models.DateTimeField(auto_now=True)
