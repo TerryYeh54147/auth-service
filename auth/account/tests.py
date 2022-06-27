9e7da7eead9d0312acd050466468169760573488
terry54147 <0930164@gmail.com>
2022-06-27 18:39:23 +0800

    add create account instance test: duplicate, losing critical key

--- auth/account/tests.py
+++ auth/account/tests.py
@@ -1,3 +1,51 @@
+from urllib import response
 from django.test import TestCase
+from .models import Account
+from rest_framework import status
+
 
 # Create your tests here.
+class AccountTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_data = {
+            'username': 'testuser',
+            'password': 'pwd1357',
+            'role': 'owner',
+            'email': 'testuser@example.com',
+            'phone': '0123456789',
+            'first_name': 'user',
+            'last_name': 'test',
+        }
+        cls.new_user=Account.objects.create_user(**cls.user_data)
+        cls.urls = {
+            'api_host': '/api/user/',
+            'login': 'login/',
+            'create': 'add/',
+        }
+
+    def get_login_token(self):
+        url = self.urls['api_host'] + self.urls['login']
+        login_data = {'username':self.user_data['username'], 'password':self.user_data['password']}
+        response = self.client.post(url, data=login_data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        token = response.json()['access']
+        return f'Bear {token}'
+
+    def create_account(self, data=None):
+        url = self.urls['api_host'] + self.urls['create']
+        header = self.get_login_token()
+        print(f'cur data: {data}')
+        return self.client.post(url, data, content_type='application/json', HTTP_AUTHORIZATION=header)
+
+    def test_create_account_duplicate(self):
+        response = self.create_account(self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+
+    def test_create_account_losing_critical_key(self):
+        data = self.user_data.copy()
+        del data['username']
+        response = self.create_account(data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    
