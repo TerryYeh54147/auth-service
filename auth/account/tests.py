7e55a93ae526cd0faa08213953dbd033de72981a
terry54147 <0930164@gmail.com>
2022-06-28 23:37:45 +0800

    add password test: less_than_min_password, empty_password, common_password, only_number_password; move the new user to setUp function

--- auth/account/tests.py
+++ auth/account/tests.py
@@ -1,6 +1,7 @@
 import random
 
 from django.test import TestCase
+from django.conf import settings
 from rest_framework import status
 
 from .models import Account
@@ -19,7 +20,6 @@ class AccountTest(TestCase):
             'first_name': 'user',
             'last_name': 'test',
         }
-        cls.new_user=Account.objects.create_user(**cls.user_data)
         cls.urls = {
             'api_host': '/api/user/',
             'login': 'login/',
@@ -27,15 +27,19 @@ class AccountTest(TestCase):
         }
         cls.rand_seed = 10
 
+    def setUp(self):
+        self.new_user = Account.objects.create_user(**self.user_data)
 
     def login_account(self, username, password):
         url = self.urls['api_host'] + self.urls['login']
         login_data = {'username': username, 'password': password}
-        response = self.client.post(url, data=login_data, content_type='application/json')
+        response = self.client.post(
+            url, data=login_data, content_type='application/json')
         return response
 
     def get_login_token(self):
-        login_data = {'username':self.user_data['username'], 'password':self.user_data['password']}
+        login_data = {
+            'username': self.user_data['username'], 'password': self.user_data['password']}
         response = self.login_account(**login_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         token = response.json()['access']
@@ -43,7 +47,8 @@ class AccountTest(TestCase):
 
     def test_login_access(self):
         msg = 'Login successful.'
-        login_data = {'username':self.user_data['username'], 'password':self.user_data['password']}
+        login_data = {
+            'username': self.user_data['username'], 'password': self.user_data['password']}
         response = self.login_account(**login_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['msg'], msg)
@@ -53,15 +58,12 @@ class AccountTest(TestCase):
         header = self.get_login_token()
         return self.client.post(url, data, content_type='application/json', HTTP_AUTHORIZATION=header)
 
-    '''
-    # TODO: solve this duplicate key issue
     def test_create_account_success(self):
         data = self.user_data.copy()
         data['username'] = 'testuser2'
         data['first_name'] = 'user2'
         response = self.create_account(data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    '''
 
     def test_create_account_duplicate(self):
         duplicate_username = self.user_data['username']
@@ -81,13 +83,62 @@ class AccountTest(TestCase):
         response = self.create_account(data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json()['msg'], msg)
-    
+
     def test_create_account_wrong_role_option(self):
         wrong_role = 'maintainers'
         msg = f'Role: {wrong_role} not found'
         data = self.user_data.copy()
         data['username'] = 'testRole'
         data['role'] = wrong_role
+        response = self.create_account(data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['msg'], msg)
+
+    def test_create_account_less_than_min_password(self, password='pwd'):
+        min_length = [validator for validator in settings.AUTH_PASSWORD_VALIDATORS if 'MinimumLengthValidator' in validator['NAME']][0].get(
+            'OPTIONS').get('min_length')
+        username = f'less_{min_length}_pwd'
+        msg = f'這個密碼過短。請至少使用 {min_length} 個字元。'
+        data = self.user_data.copy()
+        data['username'] = username
+        data['password'] = password
+        response = self.create_account(data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['msg'], msg)
+
+    def test_create_account_empty_password(self):
+        self.test_create_account_less_than_min_password(password='')
+
+    def test_create_account_common_password(self):
+        common_pwd = 'test1234'
+        msg = '這個密碼太普通。'
+        data = self.user_data.copy()
+        data['username'] = common_pwd
+        data['password'] = common_pwd
+        response = self.create_account(data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['msg'], msg)
+
+    ''' 
+    # TODO: check how to handle the similar case(UserAttributeSimilarityValidator)
+    def test_create_account_similar_password(self):
+        username, similar_pwd = 'similar_pwd', 'similar_pwd'
+        msg = f'這個密碼與使用者名稱太相近。'
+        data = self.user_data.copy()
+        data['username'] = username
+        data['password'] = similar_pwd
+        response = self.create_account(data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json()['msg'], msg)
+    '''
+
+    def test_create_account_only_number_password(self):
+        # 這個密碼只包含數字。
+        username = f'only_number_password'
+        msg = f'這個密碼只包含數字。'
+        data = self.user_data.copy()
+        data['username'] = username
+        data['password'] = '2840184892'
         response = self.create_account(data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.json()['msg'], msg)
